5c20abf5260767dccf96347e7e949546301a93bb
Andrija Novakovic <akinovak@gmail.com>
2021-08-02 18:19:15 +0200

    do not allow deploying archived repo

--- src/components/Deployment/index.ts
+++ src/components/Deployment/index.ts
@@ -57,6 +57,14 @@ export async function deployFromRequest(
   const deploymentEnv = result.project.env;
   const created = result.created;
 
+  if (project.state === 'ARCHIVED') {
+    res.status(401).json({
+      message: 'THIS REPO IS ARCHIVED AND CANNOT BE DEPLOYED'
+    });
+
+    return;
+  }
+
   if (created) {
     try {
       await DomainService.addDefault(project);
